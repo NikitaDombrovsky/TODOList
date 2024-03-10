67bebe9bf918e32943e6a47a22e2085fbd2a6d13
Nikita Dombrovskiy <nikita.nikita55@mail.ru>
2024-03-10 23:31:39 +0600

    Second Commit 2

--- app/src/main/java/com/example/todolist/data/repository/TaskRepositoryImpl.kt
+++ app/src/main/java/com/example/todolist/data/repository/TaskRepositoryImpl.kt
@@ -1,15 +1,15 @@
 package com.example.todolist.data.repository
 
-import android.content.Context
-import android.util.Log
 import com.example.todolist.data.storage.TaskEntity
 import com.example.todolist.data.storage.TaskStorage
+import com.example.todolist.data.storage.fromModel
+import com.example.todolist.data.storage.toModel
 import com.example.todolist.domain.models.TaskModel
 import com.example.todolist.domain.repository.TaskRepository
 
 class TaskRepositoryImpl(private val taskStorage: TaskStorage) : TaskRepository {
     override fun getAllTasks(): List<TaskModel> =
-        taskStorage.getAllTasks().map { taskEntity -> taskEntity.toModel() }
+        taskStorage.getAllTasks().map { taskEntity -> taskEntity.toModel()}
 
     override fun getTaskDetails(): TaskModel=
         taskStorage.getTaskDetails().toModel()
